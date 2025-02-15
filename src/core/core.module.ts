5aeb191b7c95b6e302b18dfb598574dfbbecd10e
Kennix88 <shel.live@yandex.ru>
2025-02-15 19:07:05 +0800

    add i18n

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -10,6 +10,13 @@ import { RedisModule } from '@core/redis/redis.module'
 import { ApolloDriver } from '@nestjs/apollo'
 import { GraphQLModule } from '@nestjs/graphql'
 import { IS_DEV_ENV } from '@shared/utils/is-dev.util'
+import {
+	AcceptLanguageResolver,
+	HeaderResolver,
+	I18nJsonLoader,
+	I18nModule,
+	QueryResolver,
+} from 'nestjs-i18n'
 import { LoggerModule } from 'nestjs-pino'
 
 @Module({
@@ -24,6 +31,24 @@ import { LoggerModule } from 'nestjs-pino'
 			imports: [ConfigModule],
 			useFactory: graphQLConfig,
 			inject: [ConfigService],
+		}),
+		I18nModule.forRootAsync({
+			useFactory: () => ({
+				disableMiddleware: true,
+				fallbackLanguage: 'en',
+				loaderOptions: {
+					path: 'src/core/i18n/locales',
+					watch: true,
+					includeSubfolders: true,
+				},
+				typesOutputPath: 'src/core/i18n/i18n.type.ts',
+			}),
+			resolvers: [
+				{ use: QueryResolver, options: ['lang'] },
+				AcceptLanguageResolver,
+				new HeaderResolver(['x-lang']),
+			],
+			loader: I18nJsonLoader,
 		}),
 		PrismaConnectModule,
 		RedisModule,
--- src/integrations/telegram/start.update.ts
+++ src/integrations/telegram/start.update.ts
@@ -1,5 +1,7 @@
+import { I18nTranslations } from '@core/i18n/i18n.type'
 import { Context } from '@integrations/telegram/types/telegrafContext.interface'
 import { ConfigService } from '@nestjs/config'
+import { I18nService } from 'nestjs-i18n'
 import { PinoLogger } from 'nestjs-pino'
 import { Ctx, Start, Update } from 'nestjs-telegraf'
 
@@ -8,6 +10,7 @@ export class StartUpdate {
 	constructor(
 		private readonly configService: ConfigService,
 		private readonly logger: PinoLogger,
+		private readonly i18n: I18nService<I18nTranslations>,
 	) {
 		this.logger.setContext(StartUpdate.name)
 	}
@@ -17,7 +20,11 @@ export class StartUpdate {
 		try {
 			if (ctx.chat?.type !== 'private' || !ctx.from) return
 
-			await ctx.replyWithHTML('started')
+			await ctx.replyWithHTML(
+				this.i18n.t('telegraf.start.welcome', {
+					...(ctx.from.language_code && { lang: ctx.from.language_code }),
+				}),
+			)
 
 			return
 		} catch (e) {
