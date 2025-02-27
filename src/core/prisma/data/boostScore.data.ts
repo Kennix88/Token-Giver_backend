17c3519d884c5926e485b74ea4929d3682ada4c0
Kennix88 <shel.live@yandex.ru>
2025-02-27 22:00:36 +0800

    add boostScore data - Initial data for the database

---
+++ src/core/prisma/data/boostScore.data.ts
@@ -0,0 +1,50 @@
+export const boostScoreData = [
+	{
+		priceInTgStars: 50,
+		coins: 10_000,
+		discount: 1,
+		bonus: 0,
+	},
+	{
+		priceInTgStars: 100,
+		coins: 25_000,
+		discount: 1,
+		bonus: 0,
+	},
+	{
+		priceInTgStars: 150,
+		coins: 50_000,
+		discount: 1,
+		bonus: 0,
+	},
+	{
+		priceInTgStars: 250,
+		coins: 100_000,
+		discount: 1,
+		bonus: 0,
+	},
+	{
+		priceInTgStars: 700,
+		coins: 300_000,
+		discount: 1,
+		bonus: 0,
+	},
+	{
+		priceInTgStars: 1_000,
+		coins: 500_000,
+		discount: 1,
+		bonus: 0,
+	},
+	{
+		priceInTgStars: 1_500,
+		coins: 1_000_000,
+		discount: 0.9,
+		bonus: 100_000,
+	},
+	{
+		priceInTgStars: 3_000,
+		coins: 5_000_000,
+		discount: 0.9,
+		bonus: 500_000,
+	},
+]
