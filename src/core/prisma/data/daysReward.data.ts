a551a13083bd1dbb1afab56ae147c9b96b3bae76
Kennix88 <shel.live@yandex.ru>
2025-03-01 10:26:04 +0800

    add DaysReward data - Initial data for the database

---
+++ src/core/prisma/data/daysReward.data.ts
@@ -0,0 +1,157 @@
+export const DaysRewardData = [
+	{
+		day: 1,
+		coins: 1_000,
+		gems: 0,
+	},
+	{
+		day: 2,
+		coins: 1_200,
+		gems: 0,
+	},
+	{
+		day: 3,
+		coins: 1_500,
+		gems: 0,
+	},
+	{
+		day: 4,
+		coins: 2_000,
+		gems: 0,
+	},
+	{
+		day: 5,
+		coins: 2_500,
+		gems: 1,
+	},
+	{
+		day: 6,
+		coins: 3_000,
+		gems: 0,
+	},
+	{
+		day: 7,
+		coins: 3_500,
+		gems: 0,
+	},
+	{
+		day: 8,
+		coins: 4_000,
+		gems: 0,
+	},
+	{
+		day: 9,
+		coins: 5_000,
+		gems: 0,
+	},
+	{
+		day: 10,
+		coins: 7_500,
+		gems: 1,
+	},
+	{
+		day: 11,
+		coins: 9_000,
+		gems: 0,
+	},
+	{
+		day: 12,
+		coins: 10_000,
+		gems: 0,
+	},
+	{
+		day: 13,
+		coins: 12_000,
+		gems: 0,
+	},
+	{
+		day: 14,
+		coins: 13_000,
+		gems: 0,
+	},
+	{
+		day: 15,
+		coins: 15_000,
+		gems: 2,
+	},
+	{
+		day: 16,
+		coins: 18_000,
+		gems: 0,
+	},
+	{
+		day: 17,
+		coins: 20_000,
+		gems: 0,
+	},
+	{
+		day: 18,
+		coins: 24_000,
+		gems: 0,
+	},
+	{
+		day: 19,
+		coins: 28_000,
+		gems: 0,
+	},
+	{
+		day: 20,
+		coins: 30_000,
+		gems: 2,
+	},
+	{
+		day: 21,
+		coins: 33_000,
+		gems: 0,
+	},
+	{
+		day: 22,
+		coins: 35_000,
+		gems: 0,
+	},
+	{
+		day: 23,
+		coins: 40_000,
+		gems: 0,
+	},
+	{
+		day: 24,
+		coins: 45_000,
+		gems: 0,
+	},
+	{
+		day: 25,
+		coins: 50_000,
+		gems: 3,
+	},
+	{
+		day: 26,
+		coins: 60_000,
+		gems: 1,
+	},
+	{
+		day: 27,
+		coins: 70_000,
+		gems: 1,
+	},
+	{
+		day: 28,
+		coins: 80_000,
+		gems: 1,
+	},
+	{
+		day: 29,
+		coins: 90_000,
+		gems: 1,
+	},
+	{
+		day: 30,
+		coins: 300_000,
+		gems: 30,
+	},
+	{
+		day: 31,
+		coins: 100_000,
+		gems: 3,
+	},
+]
